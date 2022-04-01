cb75bde987b950d7cb8b8394c7fb32ebf06f603d
mohamedelmnegged <mohamedaahmed92@gmail.com>
2022-04-01 15:27:22 +0200

    add validation in reservation controller and custom validation

---
+++ Diarna/Diarna/DTOs/CustomValidations/ValidateDateAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Diarna.DTOs.CustomValidations
+{
+    public class ValidateDateAttribute : ValidationAttribute
+    {
+        private readonly string comparsionAtrr;
+
+        public ValidateDateAttribute(string comparsionAtrr)
+        {
+            this.comparsionAtrr = comparsionAtrr;
+        }
+        /// <summary>
+        /// التحقق من التاريخ الابتدائي والنهائي  
+        /// وتوضع فوق التاريخ الابتدائي
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="validationContext"></param>
+        /// <returns>check of two dates</returns>
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string msg =  ErrorMessageString;
+            var startDate = ((DateTime) value).ToShortDateString();
+            var property =  validationContext.ObjectType.GetProperty(comparsionAtrr);
+            if (property == null)
+                return new ValidationResult(ErrorMessage = "لابد ان تقوم بإداخل التاريخ النهائي");
+
+            var endDate = ((DateTime)property.GetValue(validationContext.ObjectInstance)).ToShortDateString();
+            if (DateTime.Compare(DateTime.Parse(startDate), DateTime.Parse(endDate)) > 0)
+                return new ValidationResult(ErrorMessage = msg); // "لابد ان يكون التاريخ النهائي اكبر من الابتدائي");
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- Diarna/Diarna/DTOs/Reservation/ReserveUnitDto.cs
+++ Diarna/Diarna/DTOs/Reservation/ReserveUnitDto.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Diarna.DTOs.CustomValidations;
 
 namespace Diarna.DTOs.Rerservation
 {
@@ -10,13 +12,35 @@ namespace Diarna.DTOs.Rerservation
     /// </summary>
     public class ReserveUnitDto
     {
+        [Required(ErrorMessage = "الوحده مطلوبه")]
+        [ValidateValueGreaterThanZero(ErrorMessage = "رقم الوحده غير صحيح")]
         public int UnitId { get; set; }
+        
+        [Required(ErrorMessage = "اسم المستأجر مطلوب")]
         public string RentUserName { get; set; }
+        
+        [Required(ErrorMessage = "رقم الهاتف مطلوب")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\d{11})$", ErrorMessage = "عذا رقم هاتف غير صحيح")]
         public string Mobile { get; set; }
+
+        [Required(ErrorMessage = "قيمة التأمين")]
+        [ValidateValueGreaterThanZero(ErrorMessage = "قيمه التأمين يجب ان تكون اكبر من صفر")]
         public decimal? InsuranceValue { get; set; }
+
+        [Required(ErrorMessage = "قيمه الإيجار مطلوبه")]
+        [ValidateValueGreaterThanZero(ErrorMessage = "القيمه الكليه للأيجار يجب أن تكون أكبر من صفر")]
         public decimal? TotalValue { get; set; }
+
+        [Required(ErrorMessage = "قيمة تأكيد الحجز")]
+        [ValidateValueGreaterThanZero(ErrorMessage = "قيمه تأكيد الحجز يجب أن تكون أكبر من صفر")]
         public decimal? DepositValue { get; set; }
+
+        [Required(ErrorMessage = "تاريخ بدايه الحجز مطلوب")] 
+        [ValidateDate("EndDate", ErrorMessage = "لابد ان يكون التاريخ النهائي اكبر من الابتدائي")]
         public DateTime StartDate { get; set; }
+        
+        [Required(ErrorMessage = "تاريخ نهاية الحجز مطلوب")]
         public DateTime EndDate { get; set; }
 
 
